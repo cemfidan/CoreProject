0d2b6c70ca134514b7c69daa7f6d4730bd6afb7f
Cem F <aerol03@hotmail.com>
2024-01-07 15:02:13 +0300

    added about section and business operations

---
+++ BusinessLayer/Concrete/FeatureManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class FeatureManager : IGenericService<Feature>
+    {
+        IFeatureDal _featureDal;
+
+        public FeatureManager(IFeatureDal featureDal)
+        {
+            _featureDal = featureDal;
+        }
+
+        public void Add(Feature entity)
+        {
+            _featureDal.Add(entity);
+        }
+
+        public void Delete(Feature entity)
+        {
+            _featureDal.Delete(entity);
+        }
+
+        public List<Feature> GetAll()
+        {
+            return _featureDal.GetAll();
+        }
+
+        public Feature GetById(int id)
+        {
+            return _featureDal.GetById(id);
+        }
+
+        public void Update(Feature entity)
+        {
+            _featureDal.Update(entity);
+        }
+    }
+}
